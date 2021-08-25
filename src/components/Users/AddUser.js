71bd32fbef71bf058b6c1a9e90c1b89da2b91b39
ckashby <clay.ashby@gmail.com>
2021-08-25 13:37:56 -0400

    AddUser.css

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,7 +1,24 @@
 import React from "react";
+// import "./AddUser.css";
 
 const AddUser = (props) => {
-  return <h1>Add User</h1>;
+  const onSub = () => {
+    alert("Hello");
+  };
+  return (
+    <>
+      <h1 className="">Add User</h1>
+      <form onSubmit={onSub}>
+        <label>Name: </label>
+        <input type="text" placeholder="Enter name" />
+        <br />
+        <label>Age: </label>
+        <input type="number" placeholder="Enter age" />
+        <br />
+        <button type="submit">Create new user</button>
+      </form>
+    </>
+  );
 };
 
 export default AddUser;
