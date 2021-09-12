6b6e36ec65762350d4b48b2234dacc22eb733fb9
ckashby <clay.ashby@gmail.com>
2021-09-12 09:07:32 -0400

    Wrapp MeetupItem with Card

--- src/components/meetups/MeetupItem/MeetupItem.js
+++ src/components/meetups/MeetupItem/MeetupItem.js
@@ -1,20 +1,23 @@
+import Card from "../../UI/Card/Card";
 import classes from "./MeetupItem.module.css";
 
 const MeetupItem = (props) => {
   return (
     <>
       <li className={classes.item}>
-        <div className={classes.image}>
-          <img src={props.image} alt={props.image} />
-        </div>
-        <div className={classes.content}>
-          <h3>{props.title}</h3>
-          <address>{props.address}</address>
-          <p>{props.description}</p>
-        </div>
-        <div className={classes.actions}>
-          <button>Add to favorites</button>
-        </div>
+        <Card>
+          <div className={classes.image}>
+            <img src={props.image} alt={props.image} />
+          </div>
+          <div className={classes.content}>
+            <h3>{props.title}</h3>
+            <address>{props.address}</address>
+            <p>{props.description}</p>
+          </div>
+          <div className={classes.actions}>
+            <button>Add to favorites</button>
+          </div>
+        </Card>
       </li>
     </>
   );
