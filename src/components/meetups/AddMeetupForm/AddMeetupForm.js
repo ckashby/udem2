8dd8170450fccb708a04d30bc1114d11f504d39f
ckashby <clay.ashby@gmail.com>
2021-09-14 07:10:54 -0400

    Fixed classname N typo

--- src/components/meetups/AddMeetupForm/AddMeetupForm.js
+++ src/components/meetups/AddMeetupForm/AddMeetupForm.js
@@ -1,7 +1,29 @@
+import Card from "../../UI/Card/Card";
+import classes from "./AddMeetupForm.module.css";
+
 const AddMeetupForm = (props) => {
   return (
     <>
-      <h3>Add Meetup Form</h3>
+      <Card>
+        <form className={classes.form}>
+          <div className={classes.control}>
+            <label htmlFor="title">Title: </label>
+            <input type="text" id="title" />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="image">Image - URL: </label>
+            <input type="url" required id="image" />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="address">Address: </label>
+            <input type="text" required id="address" />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="description">Description: </label>
+            <input type="text" required id="description" />
+          </div>
+        </form>
+      </Card>
     </>
   );
 };
